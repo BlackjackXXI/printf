6b9923162924dd368d2460c3434794febf2d2c06
HINDOGRI <hindogri723@gmail.com>
2023-10-16 15:47:28 -0700

    putspecial updated file

--- _putspecial.c
+++ _putspecial.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _putnospec - Prints a string to standard output
+ * _putnospecifier - Prints a string to stdr output
  * @str: The string to be printed
  *
  * Return: Length of string printed - 1
  */
-int _putnospec(char *str)
+int _putnospecifier(char *str)
 {
 	int i, len = 0;
 
@@ -16,44 +16,44 @@ int _putnospec(char *str)
 	{
 		if (str[i] < 32 || str[i] >= 127)
 		{
-			len += _putchar(92);
-			len += _putchar('x');
-			len += _puthex2(str[i]);
+			len += _putcharacter(92);
+			len += _putcharacter('x');
+			len += _puthexadecimal2(str[i]);
 			continue;
 		}
-		len += _putchar(str[i]);
+		len += _putcharacter(str[i]);
 	}
 
 	return (len);
 }
 
 /**
- * _puthex2 - Prints an hex to standard output
- * @num: The number to be printed
+ * _puthexadecimal2 - Prints a hexadecimal to stdr output
+ * @numb: The number
  * Return: Length of string printed - 1
  */
-int _puthex2(int num)
+int _puthexadecimal2(int num)
 {
 	int val;
 
-	if (num < 10)
+	if (numb < 10)
 	{
-		_putchar('0');
-		_putchar(num + '0');
+		_putcharacter('0');
+		_putcharacter(numb + '0');
 		return (2);
 	}
-	if (num < 16)
+	if (numb < 16)
 	{
-		_putchar('0');
-		_putchar(num + 55);
+		_putcharacter('0');
+		_putcharacter(numb + 55);
 		return (2);
 	}
 
-	val = num % 16;
-	num = num / 16;
+	val = numb % 16;
+	num = numb / 16;
 
-	_puthex(num, 'X');
-	_puthex(val, 'X');
+	_puthexadecimal(numb, 'X');
+	_puthexadecimal(val, 'X');
 
 	return (2);
 }
